ebc6513701212d9b310d9ae8ade575c530881e32
preetisharma383 <sharma383preeti@gmail.com>
2025-11-02 14:00:55 +0530

    updated url

--- client/src/Actions/index.js
+++ client/src/Actions/index.js
@@ -4,7 +4,7 @@ export async function explain(prevState,formData){
     const code=formData.get("code");
     const language=formData.get("language");
     try {
-       const res= await fetch(`${import.meta.env.VITE_API_BASE_URL}/explain-code`,{
+       const res= await fetch(`${import.meta.env.VITE_API_BASE_URL}api/explain-code`,{
             method:'POST',
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify({code,language})
